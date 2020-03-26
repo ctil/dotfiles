26bbd05fb35805d5868f755a7cb92cc0b3980fec
ctil <ctil@users.noreply.github.com>
2020-03-26 12:25:03 -0600

    Fix tmux

--- symlink_configs.sh
+++ symlink_configs.sh
@@ -2,6 +2,6 @@
 
 ln -sf $PWD/.agignore ~
 ln -sf $PWD/.vimrc ~
-ln -sf $PWD/tmux.conf ~
+ln -sf $PWD/.tmux.conf ~
 ln -sf $PWD/aliases.zsh ~/.oh-my-zsh/custom
 ln -sf $PWD/settings.json ~/Library/Application\ Support/Code/User
