0a4abbf0ec63f7d1da4dcd109d7acd43c7951e7a
ctil <ctil@users.noreply.github.com>
2020-03-26 11:52:21 -0600

    Add more symlinks

--- symlink_configs.sh
+++ symlink_configs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/.agignore ~
+ln -sf $PWD/.vimrc ~
+ln -sf $PWD/tmux.conf ~
 ln -sf $PWD/aliases.zsh ~/.oh-my-zsh/custom
 ln -sf $PWD/settings.json ~/Library/Application\ Support/Code/User
