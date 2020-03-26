febd800ebf14d0e54c495f2b2b6db52a1ddf963f
ctil <ctil@users.noreply.github.com>
2020-03-26 15:17:51 -0600

    Add zshrc

--- symlink_configs.sh
+++ symlink_configs.sh
@@ -3,5 +3,6 @@
 ln -sf $PWD/.agignore ~
 ln -sf $PWD/.vimrc ~
 ln -sf $PWD/.tmux.conf ~
+ln -sf $PWD/.zshrc ~
 ln -sf $PWD/aliases.zsh ~/.oh-my-zsh/custom
 ln -sf $PWD/settings.json ~/Library/Application\ Support/Code/User
