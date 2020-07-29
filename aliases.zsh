8ac2118e31620a20379456ab33d3b4a02c612b2b
ctil <ctil@users.noreply.github.com>
2020-07-29 15:03:04 -0600

    Update aliases. Fix ripgreprc

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,7 @@ alias bbbbb="cd ../../../../../"
 alias wk="cd ~/work"
 
 alias ltr="ls -ltr"
+alias f="find . -name"
 
 # Git
 alias gam="git commit -a --amend --no-edit"
@@ -19,3 +20,6 @@ if [ "$(uname 2> /dev/null)" = "Linux" ]; then
     alias pbpaste="xclip -selection clipboard -o"
 fi
 
+alias sand='cd /Users/colin/work/sandbox && cargo run'
+alias mr='cd /Users/colin/work/monorepo'
+alias rust="evcxr"
