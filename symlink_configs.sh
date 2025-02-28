22dd1eac112b920c521fffc2c4fe5202b5e0c89b
ctil <ctil@users.noreply.github.com>
2025-02-27 19:02:13 -0700

    I ain't 'fraid of no ghostty

--- symlink_configs.sh
+++ symlink_configs.sh
@@ -7,3 +7,5 @@ ln -sf $PWD/.ripgreprc ~
 ln -sf $PWD/aliases.zsh ~/.oh-my-zsh/custom
 ln -sf $PWD/settings.json ~/Library/Application\ Support/Code/User
 ln -sf $PWD/keybindings.json ~/Library/Application\ Support/Code/User
+mkdir -p ~/.config/ghostty
+ln -sf $PWD/ghostty_config ~/.config/ghostty/config
