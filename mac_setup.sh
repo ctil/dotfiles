07ecd8650633ae7f79256d9ede7734bb419be52b
ctil <ctil@users.noreply.github.com>
2025-03-18 21:40:50 -0600

    Add missing steps to mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # Install tools
-brew install fzf ripgrep starship ghostty
+brew install fzf \
+    gh \
+    ripgrep \
+    starship \
+    ghostty \
+    tmux \
+    zoxide \
+    nvim
+
+if [ ! -d "$HOME/.config/nvim" ]; then
+    git clone https://github.com/ctil/nvim.git ~/.config/nvim
+fi
+
+if [ ! -d "$HOME/.config/.nvm" ]; then
+    git clone https://github.com/nvm-sh/nvm.git .nvm
+fi
 
 # Enable key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
