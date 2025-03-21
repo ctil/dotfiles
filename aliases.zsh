02aa3e759aad6a4d357149d020e6c34b9657b628
ctil <ctil@users.noreply.github.com>
2025-03-21 10:42:57 -0600

    Add some tmux aliases

--- aliases.zsh
+++ aliases.zsh
@@ -9,6 +9,7 @@ alias ltr="ls -ltr"
 alias f="rg --files -g "
 alias j="just"
 alias c="clear"
+alias sz="source ~/.zshrc"
 
 # Git
 alias gam="git commit -a --amend --no-edit"
@@ -21,8 +22,13 @@ if [ "$(uname 2> /dev/null)" = "Linux" ]; then
     alias pbpaste="xclip -selection clipboard -o"
 fi
 
-alias mr='cd ~/code/monorepo && nvm use'
 alias notify="~/notify_done.sh"
 alias vi="nvim"
 
+# Wyyerd Stuff
 alias dm="diesel migration"
+alias mr='cd ~/code/monorepo && nvm use'
+
+# Tmux
+alias tn="tmux new-session -s "
+alias tk="tmux kill-session -t "
